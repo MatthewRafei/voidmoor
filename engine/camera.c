9683a16d40a93e382bff6d78649f8243034c51bf
MatthewRafei <matthewrafei1@gmail.com>
2026-06-19 12:06:39 -0500

    Starting fresh

--- engine/camera.c
+++
@@ -1,9 +0,0 @@
-#include "camera.h"
-#include "player.h"
-#include "render.h"
-
-void update_camera_position(Camera *camera, Player player)
-{
-    camera->x = (player.tx - player.ty) * TILE_WIDTH_HALF;
-    camera->y = (player.tx + player.ty) * TILE_HEIGHT_HALF;
-}
